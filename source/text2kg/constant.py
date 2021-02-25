4041f847845d98205b1ae57fdd8a3f3e886e7fe2
okanciftci <>
2021-02-26 01:22:14 +0300

    text2kg starter added

---
+++ source/text2kg/constant.py
@@ -0,0 +1,30 @@
+found_invalid = []
+
+invalid_relations = [] + found_invalid
+
+auxiliaries = []
+
+with open('../../corpus/trnlp-data/tr_adj.txt', 'r') as f:
+    adjectives_trnlp = [ line.strip().lower() for line in f]
+
+with open('../../corpus/trnlp-data/tr_adverb.txt', 'r') as f:
+    adverbs_trnlp = [ line.strip().lower() for line in f]
+
+with open('../../corpus/wordnet-data/tr_adjectives.txt', 'r') as f:
+    adjectives_wordnet = [ line.strip().lower() for line in f]
+
+with open('../../corpus/wordnet-data/tr_adverbs.txt', 'r') as f:
+    adverbs_wordnet = [ line.strip().lower() for line in f]
+
+
+# with open('corpus/Wordlist-Verbs-All.txt', 'r') as f:
+#     verbs = [ line.strip().lower() for line in f]
+
+invalid_relations += adverbs_wordnet
+invalid_relations += adverbs_trnlp
+invalid_relations += adjectives_wordnet
+invalid_relations += adjectives_trnlp
+
+# invalid_relations += verbs
+
+invalid_relations_set = set(invalid_relations)
---
+++ source/text2kg/extract.py
@@ -0,0 +1,84 @@
+
+import sys, os
+from process import parse_sentence
+from mapper import Map, deduplication
+from transformers import AutoTokenizer, BertModel
+import argparse
+import en_core_web_md
+from tqdm import tqdm
+import json
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser = argparse.ArgumentParser(description='Process lines of text corpus into knowledgraph')
+parser.add_argument('input_filename', type=str, help='text file as input')
+parser.add_argument('output_filename', type=str, help='output text file')
+parser.add_argument('--language_model',default='bert-base-turkish-cased', 
+                    choices=[ 'bert-base-turkish-cased', 'bert-base-turkish-uncased'],
+                    help='which language model to use')
+parser.add_argument('--use_cuda', default=True, 
+                        type=str2bool, nargs='?',
+                        help="Use cuda?")
+parser.add_argument('--include_text_output', default=False, 
+                        type=str2bool, nargs='?',
+                        help="Include original sentence in output")
+parser.add_argument('--threshold', default=0.003, 
+                        type=float, help="Any attention score lower than this is removed")
+
+args = parser.parse_args()
+
+use_cuda = args.use_cuda
+nlp = en_core_web_md.load()
+
+
+
+language_model = args.language_model
+
+
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained(language_model)
+    encoder = BertModel.from_pretrained(language_model)
+    encoder.eval()
+    if use_cuda:
+        encoder = encoder.cuda()    
+    input_filename = args.input_filename
+    output_filename = args.output_filename
+    include_sentence = args.include_text_output
+
+    with open(input_filename, 'r') as f, open(output_filename, 'w') as g:
+        for idx, line in enumerate(tqdm(f)):
+            sentence  = line.strip()
+            if len(sentence):
+                valid_triplets = []
+                for sent in nlp(sentence).sents:
+                    # Match
+                    for triplets in parse_sentence(sent.text, tokenizer, encoder, nlp, use_cuda=use_cuda):
+                        valid_triplets.append(triplets)
+                if len(valid_triplets) > 0:
+                    # Map
+                    mapped_triplets = []
+                    for triplet in valid_triplets:
+                        head = triplet['h']
+                        tail = triplet['t']
+                        relations = triplet['r']
+                        conf = triplet['c']
+                        if conf < args.threshold:
+                            continue
+                        mapped_triplet = Map(head, relations, tail)
+                        if 'h' in mapped_triplet:
+                            mapped_triplet['c'] = conf
+                            mapped_triplets.append(mapped_triplet)
+                    output = { 'line': idx, 'tri': deduplication(mapped_triplets) }
+
+                    if include_sentence:
+                        output['sent'] = sentence
+                    if len(output['tri']) > 0:
+                        g.write(json.dumps( output )+'\n')
