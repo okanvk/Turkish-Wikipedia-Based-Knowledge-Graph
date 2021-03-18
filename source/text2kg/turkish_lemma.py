243d26335cf1cee997f3f983e47cd6fb4632d7a7
atesbilgin <atesbilgin07@gmail.com>
2021-03-18 20:18:58 +0300

    zeyrek lemmatizer combined with spark nlp

--- source/text2kg/turkish_lemma.py
+++ source/text2kg/turkish_lemma.py
@@ -1,12 +1,56 @@
+import sys
 
+from pyspark.sql import Row
+from pyspark.sql import SparkSession
+from pyspark.ml import PipelineModel
+
+import sparknlp
+from sparknlp.annotator import *
+from sparknlp.common import *
+from sparknlp.base import *
+from sparknlp.pretrained import ResourceDownloader
+import time
+from pathlib import Path
 import zeyrek
+
+if sys.version_info[0] < 3:
+    from urllib import urlretrieve
+else:
+    from urllib.request import urlretrieve
+    
+spark = sparknlp.start()
+
 class TurkishLemmatizer():
 
 
     def __init__(self):
         self.analyzer = zeyrek.MorphAnalyzer()
         
+        self.documentAssembler = DocumentAssembler() \
+            .setInputCol("sentence") \
+            .setOutputCol("document")
+
+        self.tokenizer = Tokenizer() \
+            .setInputCols(["document"]) \
+            .setOutputCol("token")
+
+        self.lemmatizer = LemmatizerModel.pretrained("lemma","tr") \
+            .setInputCols(["token"]) \
+            .setOutputCol("lemma")
+
+        self.finisher = Finisher() \
+            .setInputCols([ "lemma"]) \
+            .setIncludeMetadata(True)
+
+        self.pipeline_fast_dl = Pipeline(stages = [
+            self.documentAssembler, 
+            self.tokenizer, 
+            self.lemmatizer, 
+            self.finisher])
+        
     def bring_lemma(self,word):
+        
+        R = Row('sentence')    
         
         result_index = 0
         lemma_index_in_tuple = 1
@@ -16,9 +60,18 @@ class TurkishLemmatizer():
             try:
                 res = analysis[result_index][lemma_index_in_tuple]
                 if len(res) == 0:
-                    return [word]
+                    data = spark.createDataFrame([R(word)])
+                    m = self.pipeline_fast_dl.fit(data).transform(data)
+                    sparkRes = m.select('sentence').collect()[0].sentence
+                    return [sparkRes]
                 return res
             except IndexError:
-                return [word]
-        return [word]
+                data = spark.createDataFrame([R(word)])
+                m = self.pipeline_fast_dl.fit(data).transform(data)
+                sparkRes = m.select('sentence').collect()[0].sentence
+                return [sparkRes]
+        data = spark.createDataFrame([R(word)])
+        m = self.pipeline_fast_dl.fit(data).transform(data)
+        sparkRes = m.select('sentence').collect()[0].sentence
+        return [sparkRes]
 
