da733eb4be3003c5a4d4f195e29a5f9d6a0744c5
okanciftci <>
2021-03-20 00:04:07 +0300

    stanza added

--- source/text2kg/turkish_lemma.py
+++ source/text2kg/turkish_lemma.py
@@ -1,19 +1,33 @@
+
 import zeyrek
+import stanza
+
+stanza.download('tr')
+
 class TurkishLemmatizer():
 
 
     def __init__(self):
         self.analyzer = zeyrek.MorphAnalyzer()
+        self.nlp = stanza.Pipeline(lang='tr', processors='tokenize,lemma')
 
+    def _bringStanzaLemma(self,word):
+        doc = self.nlp(word)
+        first_lemma = [word.lemma for sent in doc.sentences for word in sent.words][0]
+        return first_lemma
 
     def bring_lemma(self,word):
         result_index = 0
         lemma_index_in_tuple = 1
         try:
             res = analysis[result_index][lemma_index_in_tuple]
-            if len(res) == 0:
-                return [word]
+            if len(res) == 0 or len(res) >= 1:
+                return self._bringStanzaLemma(word)
             return res
         except IndexError:
-            return [word]
-        return [word]
+            return self._bringStanzaLemma(word)
+        return self._bringStanzaLemma(word)
+
+a = TurkishLemmatizer()
+
+print(a._bringStanzaLemma("kitapçılarda"))
