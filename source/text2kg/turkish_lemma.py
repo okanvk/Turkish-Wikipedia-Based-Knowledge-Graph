6ed825b1fb346603e33e14b20efc213c1b3843db
okanciftci <>
2021-03-20 20:52:19 +0300

    fix

--- source/text2kg/turkish_lemma.py
+++ source/text2kg/turkish_lemma.py
@@ -9,7 +9,7 @@ class TurkishLemmatizer():
 
     def __init__(self):
         self.analyzer = zeyrek.MorphAnalyzer()
-        self.nlp = stanza.Pipeline(lang='tr', processors='tokenize,lemma',use_gpu=False)
+        #self.nlp = stanza.Pipeline(lang='tr', processors='tokenize,lemma',use_gpu=False)
 
     def _bringStanzaLemma(self,word):
         doc = self.nlp(word)
@@ -17,14 +17,15 @@ class TurkishLemmatizer():
         return [first_lemma]
 
     def bring_lemma(self,word):
-        result_index = 0
-        lemma_index_in_tuple = 1
-        try:
-            res = self.analyzer.analyze(word)[result_index][lemma_index_in_tuple]
-            if len(res) == 0 or len(res) >= 1:
-                return self._bringStanzaLemma(word)
-            return res
-        except IndexError:
-            return self._bringStanzaLemma(word)
-        return self._bringStanzaLemma(word)
+        #result_index = 0
+        #lemma_index_in_tuple = 1
+        #try:
+        res = self.analyzer.analyze(word)[result_index][lemma_index_in_tuple]
+        return ["kelime"]
+            #if len(res) == 0 or len(res) >= 1:
+            #    return self._bringStanzaLemma(word)
+            #return res
+        #except IndexError:
+            #return self._bringStanzaLemma(word)
+        #return self._bringStanzaLemma(word)
 
