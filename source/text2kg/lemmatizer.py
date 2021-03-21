b773e9718b9d86ddcea4be8c51fb4b6922910c5d
enelpi <okanvk.ciftci@gmail.com>
2021-03-21 06:33:47 +0300

    m

--- source/text2kg/lemmatizer.py
+++ source/text2kg/lemmatizer.py
@@ -106,6 +106,6 @@ except IOError:
 def bring_first_lemma(word):
     findings = findPos(word, revisedDict)
     if len(findings) > 0:
-        return findings[0][:-2]
+        return [findings[0][:-2]]
     else:
         return [word]
